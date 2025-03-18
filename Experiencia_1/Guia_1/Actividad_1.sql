d521ca619c35271a98a9e25c9bf557c0383e7a85
CuescoDePalta <169055877+CatalinaOrmeno@users.noreply.github.com>
2025-03-17 22:29:03 -0300

    Avance_guia_1
    - Caso 6: terminado.
    - Caso 4: sin hacer.
    Lo voy a completar cuando el profe de la solución mañana.

--- Experiencia_1/Guia_1/Actividad_1.sql
+++ Experiencia_1/Guia_1/Actividad_1.sql
@@ -1,8 +1,8 @@
 --Caso 1:
 SELECT
     numrun_cli||'-'||dvrun_cli "RUN cliente",
-    Lower(pnombre_cli||' '||snombre_cli)||' '||appaterno_cli||' '||apmaterno_cli "Nombre completo",
-    to_char(fecha_nac_cli,'DD/MM/YYYY') "Fecha de nacimiento"
+    Lower(pnombre_cli||' '||snombre_cli)||' '||appaterno_cli||' '||apmaterno_cli "NOMBRE COMPLETO CLIENTE",
+    to_char(fecha_nac_cli,'DD/MM/YYYY') "FECHA NACIMIENTO"
 FROM cliente
 where EXTRACT(day from fecha_nac_cli)=EXTRACT(day from SYSDATE+1) and 
     EXTRACT(month from fecha_nac_cli)=EXTRACT(month from SYSDATE+1)
@@ -11,7 +11,7 @@ ORDER BY appaterno_cli;
 --Caso 2:
 SELECT
     numrun_emp||' '||dvrun_emp "RUN EMPLEADO",
-    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE COMPLETO EMLEADO",
+    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE COMPLETO EMPLEADO",
     sueldo_base "SUELDO BASE",
     trunc(sueldo_base/100000) "PORCENTAJE MOVILIZACI�N",
     round(trunc(sueldo_base/100000)/100 * sueldo_base) "VALOR MOVILIZACI�N"
@@ -21,7 +21,7 @@ order by 4 desc;
 --Caso 3:
 SELECT
     numrun_emp||' '||dvrun_emp "RUN EMPLEADO",
-    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE COMPLETO EMLEADO",
+    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE COMPLETO EMPLEADO",
     sueldo_base "SUELDO BASE",
     to_char(fecha_nac,'DD/MM/YYYY') "FECHA NACIMIENTO",
     SUBSTR(pnombre_emp,1,3)||length(pnombre_emp)||'*'||substr(sueldo_base,-1)||dvrun_emp||round(months_between(sysdate,fecha_contrato)/12) "NOMBRE USUARIO",
@@ -30,15 +30,51 @@ FROM empleado
 order by appaterno_emp;
 
 --Caso 4:
-
+/*
+delete table HIST_REBAJA_ARRIENDO;
+create table HIST_REBAJA_ARRIENDO(
+    anno_proceso Date not null,
+    nro_patente Varchar2(6) not null
+);*/
 --Caso 5:
-
+SELECT 
+    to_char(sysdate,'MM/YYYY') "MES_ANNO_PROCESO",
+    nro_patente,
+    to_char(fecha_ini_arriendo,'DD/MM/YYYY')"FECHA_INI_ARRIENDO",
+    dias_solicitados,
+    to_char(fecha_devolucion,'DD/MM/YYYY')"FECHA_DEVOLUCION",
+    trunc(months_between(sysdate,fecha_devolucion)/12*365) "DIAS_ATRASO",
+    trunc(months_between(sysdate,fecha_devolucion)/12*365)*&Ingrese_valor_multa "VALOR_MULTA"
+FROM arriendo_camion
+where trunc(months_between(sysdate,fecha_devolucion)/12*365) > 0
+order by fecha_ini_arriendo asc, nro_patente asc;
 --Caso 6:
+VARIABLE Utilidades int;
+EXEC :Utilidades := &Ingrese_utilidades;
+SELECT 
+    to_char(sysdate,'MM/YYYY') "FECHA PROCESO",
+    numrun_emp||'-'||dvrun_emp "RUN EMPLEADO",
+    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE EMPLEADO",
+    lpad('$'||sueldo_base,12) "SUELDO BASE",
+    case
+        when sueldo_base >= 320000 and sueldo_base <= 450000 then
+            :Utilidades * (0.5/100)
+        when sueldo_base >= 450001 and sueldo_base <= 600000 then
+            :Utilidades * (0.35/100)
+        when sueldo_base >= 600001 and sueldo_base <= 900000 then
+            :Utilidades * (0.25/100)
+        when sueldo_base >= 900001 and sueldo_base <= 1800000 then
+            :Utilidades * (0.15/100)
+        else
+            :Utilidades * (0.1/100)
+    end "BONIFICACION POR UTILIDADES"
+FROM empleado
+order by appaterno_emp;
 
 --Caso 7:
 SELECT 
     e.numrun_emp||'-'||e.dvrun_emp "RUN EMPLEADO",
-    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE COMPLETO EMLEADO",
+    pnombre_emp||' '||snombre_emp||' '||appaterno_emp||' '||apmaterno_emp "NOMBRE COMPLETO EMPLEADO",
     round(months_between(sysdate,e.fecha_contrato)/12) "AÑOS CONTRATADO",
     lpad('$'||sueldo_base,12) "SUELDO BASE",
     lpad('$'||round(trunc(sueldo_base/100000)/100 * sueldo_base),18) "VALOR MOVILIZACI�N",
