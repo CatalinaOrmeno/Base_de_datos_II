361ce2c2f4411ca2f724715393e2ba441320e66a
CuescoDePalta <169055877+CatalinaOrmeno@users.noreply.github.com>
2025-04-29 09:47:50 -0400

    Empezar_experiencia_2

---
+++ Experiencia_2/Introducción_DML.sql
@@ -0,0 +1,9 @@
+/*
+Introducción DML:
+    CRUD    |        DML
+----------------------------------
+- C: create |  Insert - Guardar
+- R: read   |  Select - Consultar
+- U: Update |  Update - Actualizar
+- D: Delete |  Delete - Eliminar
+*/
